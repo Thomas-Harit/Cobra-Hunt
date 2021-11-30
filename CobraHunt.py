52d45c8da02c417b0cec2d1284fe3365356a3b3c
pierrick marguin <pierrick.marguin@epitech.eu>
2021-11-30 23:22:44 +0100

    moving duck + get key input

--- CobraHunt.py
+++ CobraHunt.py
@@ -1,6 +1,8 @@
 import pygame
 import random
+import os
 from typing import List
+ 
 
 pygame.init()
 
@@ -10,7 +12,7 @@ window_height: int = 600
 class Duck:
     x: int = 0
     y: int = 0
-    speed: int = 0
+    speed: int = 1
     sprite: pygame.Surface
 
     def __init__(self, sprite):
@@ -21,19 +23,34 @@ class Duck:
         self.x += self.speed
     
     def display(self, window: pygame.Surface):
-        window.blit(self.sprite, self.x, self.y)
+        window.blit(self.sprite, (self.x, self.y))
 
 loop: bool = True
 
-window = pygame.display.set_mode((window_width, window_height))
-omegalili = pygame.image.load("/home/pmarguin/Bureau/omegalili/LiLinkedin.jpg")
-
-window.fill((0, 0, 0, 255))
-window.blit(omegalili, (100, 100))
-pygame.display.update()
+window = pygame.display.set_mode((window_width, window_height), pygame.RESIZABLE)
+duckSprite = pygame.image.load(os.path.join("assets", "duck.png"))
+clock = pygame.time.Clock()
 
 score = 0
 DuckList: List[Duck] = []
+
 while loop == True:
-    print("JE LOOP")
-    loop = False
+    if len(DuckList) == 0:
+        DuckList.append(Duck(duckSprite))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            loop = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed(3)[0]:
+                print("button 0 pressed at ", end='')
+            if pygame.mouse.get_pressed(3)[1]:
+                print("button 1 pressed at ", end='')
+            if pygame.mouse.get_pressed(3)[2]:
+                print("button 2 pressed at ", end='')
+            print(pygame.mouse.get_pos()[0], " ", pygame.mouse.get_pos()[1])
+    window.fill((0, 0, 0, 255))
+    for duck in DuckList:
+        duck.move()
+        duck.display(window)
+    pygame.display.update()
+    clock.tick(60)
